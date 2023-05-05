d46e768a5f9227962deb88f713be3ae27dda5a66
Damien DIDIER <damien.didier@epita.fr>
2023-05-05 16:32:09 +0200

    ADD : viscosity fit to the velocity profile

--- viscosity.m
+++ viscosity.m
@@ -1,11 +1,12 @@
 function video = viscosity(mask_cell , velocity_cell, angle_list, one_cycle_dir, filename)
 
 num_vessels = size(mask_cell, 2);
+num_frames = size(velocity_cell{1}, 3);
 n_interp = 100;
 %interpolation parameter
 k = 2;
 
-velocity_profiles = zeros(n_interp, size(velocity_cell{1}, 3), num_vessels);
+velocity_profiles = zeros(n_interp, num_frames, num_vessels);
 
 for ii = 1 : num_vessels
     subImg = mask_cell{ii};
@@ -27,7 +28,8 @@ for ii = 1 : num_vessels
         subVideo_interp(:,:, frame) = imrotate(subVideo_interp(:,:, frame), tilt_angle ,'bilinear','crop');
     end
     
-
+    
+    projVideo = sum(subVideo_interp, 3);
     % find vessels width
     projImg = squeeze(sum(subImg_interp, 1));
     projImg = projImg/max(projImg, [], "all");
@@ -62,25 +64,63 @@ v = VideoWriter(fullfile(one_cycle_dir, strcat(filename,'_velocity_profile.avi')
 open(v);
 mimin = min(average_velocity_profile(:));
 mamax = max(average_velocity_profile(:));
-for tt = 1 : size(velocity_cell{1}, 3)
+% x for normalize wall length for fiting
+x = linspace(-1,1,length(average_velocity_profile(:,1)));
+
+Vmax_list = zeros(num_frames,1);
+alpha_list = zeros(num_frames,1);
+beta_list = zeros(num_frames,1);
+eta_list = zeros(num_frames,1);
+viscosity_list = zeros(num_frames,1);
+
+for tt = 1 : num_frames
+    tmp_velocity_profile = squeeze(average_velocity_profile(:,tt));
+
+    % Use the defined function as an input to fit the function of viscosity
+    tmp_fittype = fittype('Vmax .* (1-(1-alpha).* (abs(x).^beta))',...
+    'dependent',{'tmp_velocity_profile'},'independent',{'x'},...
+    'coefficients',{'Vmax','alpha','beta'});
+    % tmp_fittype = fittype('Vmax .* (1-(1-0.13).* (abs(x).^beta))',...
+    % 'dependent',{'tmp_velocity_profile'},'independent',{'x'},...
+    % 'coefficients',{'Vmax','beta'});
+    [tmp_fit, R2_tmp_fit] = fit(x', tmp_velocity_profile, tmp_fittype, 'StartPoint', [15 0.7 2],'Lower', [5 0.3 1], 'Upper', [30 0.95 3]);
+    R2_tmp_fit = R2_tmp_fit.rsquare;
+    
     fifig = figure(899);
-    plot(squeeze(average_velocity_profile(:,tt)),'-k', 'LineWidth',2) ;
+    plot(tmp_velocity_profile,'-k', 'LineWidth',2) ;
+
+    hold on
+    plot(tmp_fit(x), '-r', 'LineWidth',2);
     title('average wall-to-wall arterial velocity profile');
-    % legend('arterial pulse','background', 'venous signal') ;
+    legend(strcat('R² = ',string(R2_tmp_fit),' Vmax = ', string(tmp_fit.Vmax),' alpha = ', string(tmp_fit.alpha),' beta = ', string(tmp_fit.beta)));
     fontsize(gca,12,"points") ;
-    xticks(0:10:100);
-    xticklabels({'0','wall start', '20','30', '40','50','60','70', '80', '90','wall end'});
+    xticks(x);
+    xticklabels({'-1','wall start', '-0.6','-0.4', '-0.2','0','0.2','0.4', '0.6', '0.8','wall end'});
     xlabel('section','FontSize',14) ;
     pbaspect([1.618 1 1]) ;
     set(gca, 'LineWidth', 2);
     axis tight;
     ylim([mimin mamax]);
     ylabel('quantitative velocity mm/s','FontSize',14) ;
+    hold off
     writeVideo(v, getframe(fifig));
+
+
+    Vmax_list(tt) = tmp_fit.Vmax;
+    alpha_list(tt) = tmp_fit.alpha;
+    % alpha_list(tt) = 0.13;
+    beta_list(tt) = tmp_fit.beta;
+    eta_list(tt) = (tmp_fit.beta + 1)/(tmp_fit.beta + tmp_fit.alpha);
+    viscosity_list(tt) = -(eta_list(tt)-1.459)/0.017;
+
 end
 close(v)
 video = subVideo;
-
-
-
+figure(666)
+plot(viscosity_list)
+pbaspect([1.618 1 1]);
+xlabel('Frame','FontSize',14);
+ylabel('Viscosity (cP)','FontSize',14); 
+set(gca, 'LineWidth', 2);
+axis tight;
 end
