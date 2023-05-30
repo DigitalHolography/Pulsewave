106a7ea68be6ed9b18667c2713548bdf6fc9ac0a
Yohan Blazy <yohan.blazy@icloud.com>
2023-05-30 12:15:32 +0200

    [FIX] read cache from mat folder

--- getTimeTransformParamsFromCache.m
+++ getTimeTransformParamsFromCache.m
@@ -4,16 +4,19 @@ myFolders = split(one_cycle_dir,'\');
 myFolders{size(myFolders,1)+1} = '..';
 myFolders{size(myFolders,1)+1} = '..';
 myFolders{size(myFolders,1)+1} = myFolders{size(myFolders,1)-4};
-%FIXME : aller dans le rep mat
+
+myReadPath = {myFolders{1},myFolders{2},'mat'};
+myReadPath = join(myReadPath,'\');
+
 myNewPath = join(myFolders,'\');
 myNewPath = [myNewPath{1},'.mat'];
 [~,filename_mat,~] = fileparts(myNewPath);
 % filename_mat = filename(1:length(filename)-3) ;
-cache_exists = exist(fullfile(strcat(strcat(one_cycle_dir,'/../../'),filename_mat,'.mat')));
+cache_exists = exist(fullfile(myReadPath{1},strcat(filename_mat,'.mat')));
 %
 if cache_exists % .mat with cache from holowaves is present, timeline can be computed
     disp('reading cache parameters');    
-    load(fullfile(strcat(strcat(one_cycle_dir,'/../../'),filename_mat,'.mat')),'cache') ;
+    load(fullfile(myReadPath{1},strcat(filename_mat,'.mat')),'cache') ; 
     stride = cache.batch_stride ;
     type = cache.time_transform.type;
     F1 = cache.time_transform.f1;
--- getTimelineParamsFromCache.m
+++ getTimelineParamsFromCache.m
@@ -4,16 +4,21 @@ myFolders = split(one_cycle_dir,'\');
 myFolders{size(myFolders,1)+1} = '..';
 myFolders{size(myFolders,1)+1} = '..';
 myFolders{size(myFolders,1)+1} = myFolders{size(myFolders,1)-4};
+
+myReadPath = {myFolders{1},myFolders{2},'mat'};
+myReadPath = join(myReadPath,'\');
+
+
 %FIXME : aller dans le rep mat
 myNewPath = join(myFolders,'\');
 myNewPath = [myNewPath{1},'.mat'];
 [~,filename_mat,~] = fileparts(myNewPath);
 % filename_mat = filename(1:length(filename)-3) ;
-cache_exists = exist(fullfile(strcat(strcat(one_cycle_dir,'/../../'),filename_mat,'.mat')));
+cache_exists = exist(fullfile(myReadPath{1},strcat(filename_mat,'.mat')));
 %
 if cache_exists % .mat with cache from holowaves is present, timeline can be computed
     disp('reading cache parameters');    
-    load(fullfile(strcat(strcat(one_cycle_dir,'/../../'),filename_mat,'.mat')),'cache') ;
+    load(fullfile(myReadPath{1},strcat(filename_mat,'.mat')),'cache') ;
     stride = cache.batch_stride ;
     Fs = cache.Fs;
     flag = 1;
