5a15e1c75e6339ddfedf269618c2a630df624f46
Jeanne Malecot <jeanne.malecot@telecom-paris.fr>
2023-10-13 15:27:48 +0200

    wrong use of Parameters_json in functions

--- cross_section_analysis_new.m
+++ cross_section_analysis_new.m
@@ -1,8 +1,6 @@
 function [avg_blood_volume_rate,std_blood_volume_rate, cross_section_area, avg_blood_velocity, cross_section_mask,total_avg_blood_volume_rate,total_std_blood_volume_rate] = cross_section_analysis_new(locs, width, mask, v_RMS, slice_half_thickness, k,ToolBox, path,fig)
 % validate_cross_section
 %   Detailed explanation goes here FIXME
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 PW_params = Parameters_json(path);
 subImg_cell = cell(size(locs,1));
