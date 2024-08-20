8bc5e9aed1fae2a94378b7a21016e1ef5ebd3295
Zacharie-any <z.auray@gmail.com>
2024-08-20 10:56:07 +0200

    dimension

--- OneCycleClass.m
+++ OneCycleClass.m
@@ -220,6 +220,8 @@ classdef OneCycleClass
                 disc_ratio = 0.7; % parametrize this coef if needed
                 disc = X.^2+Y.^2 < (disc_ratio * min(Nx,Ny)/2)^2; 
                 video_reg = video .* disc - disc .* sum(video.* disc,[1,2])/nnz(disc); % minus the mean in the disc of each frame
+                video_reg = reshape(video_reg,size(video,1),size(video,2),1,size(video,3)); % insert a dimension to match reegistration functions
+
                 video_reg = video_reg ./(max(abs(video_reg),[],[1,2])); % rescaling each frame but keeps mean at zero
                  
                 image_ref = video_reg(:,:,floor(size(video,3)/2)); % ref image is the middle frame of the video
@@ -281,6 +283,10 @@ classdef OneCycleClass
             in_height = size(obj.reference{1}, 1);
             in_width = size(obj.reference{1}, 2);
             in_num_frames = size(obj.reference{1}, 3);
+
+            if out_height < 0 && out_width < 0 && out_num_frames < 0
+                return % do nothing if not required
+            end
 
             if out_height < 0
                 out_height = in_height;
