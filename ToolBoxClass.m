b3db6564533195affee9b2bb40cdc16fd4ce0e14
Yann Fischer <yann.fischer.pro@gmail.com>
2024-10-07 10:56:11 +0200

    for all radii

--- ToolBoxClass.m
+++ ToolBoxClass.m
@@ -62,7 +62,7 @@ classdef ToolBoxClass < handle
             for i=1:length(list_dir)
                 if contains(list_dir(i).name, PW_folder_name)
                     match = regexp(list_dir(i).name, '\d+$', 'match'); 
-                    if str2double(match{1}) >= idx
+                    if ~isempty(match) && str2double(match{1}) >= idx
                         idx = str2double(match{1}) + 1; %suffix
                     end
                 end
