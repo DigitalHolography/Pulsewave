f6dea5c4cc2e6b527e1ff4f23674ffe5b5b2a428
Yann Fischer <yannfisch3r@gmail.com>
2024-12-20 11:57:05 +0100

    ToolBox cleaning

--- Tools/ToolBoxClass.m
+++ Tools/ToolBoxClass.m
@@ -17,29 +17,14 @@ classdef ToolBoxClass < handle
         PW_path_json char
         PW_path_log char
         PW_path_pulswave char
-        PW_param_name char
         main_foldername char
+        PW_param_name char
         PW_folder_name char
         stride double
         fs double
-        type char
         f1 double
         f2 double
-        minPCA double
-        maxPCA double
         ScalingFactorVelocityInPlane double
-        ScalingFactorVelocityCRA_AVG double
-        ScalingFactorVelocityCRA_RMS double
-        ARI_hue_max double
-        ARI_hue_min double
-        ARI_inflexion_point_hue double
-        ARI_slope_hue double
-        ARI_val_max double
-        ARI_val_min double
-        ARI_inflexion_point_val double
-        ARI_slope_val double
-        NormalizationFactor double
-        NormalizationOffset double
 
     end
 
@@ -54,7 +39,7 @@ classdef ToolBoxClass < handle
             %% Creating paths
             idx = 0;
             split_path = strsplit(path, '\');
-            obj.main_foldername = split_path{end - 1};
+            obj.PW_folder_name = split_path{end - 1};
             obj.PW_path_main = fullfile(path, 'pulsewave');
 
             if ~exist(obj.PW_path_main, 'dir')
@@ -78,7 +63,7 @@ classdef ToolBoxClass < handle
             %    idx = idx + 1;
             %end
 
-            obj.PW_folder_name = sprintf('%s_%d', PW_folder_name, idx);
+            obj.PW_folder_name = sprintf('%s_PW_%d', PW_folder_name, idx);
             obj.PW_path_dir = fullfile(obj.PW_path_main, obj.PW_folder_name);
             obj.PW_path_png = fullfile(obj.PW_path_dir, 'png');
             obj.PW_path_eps = fullfile(obj.PW_path_dir, 'eps');
@@ -109,11 +94,8 @@ classdef ToolBoxClass < handle
                 load(file_path_mat, 'cache');
                 obj.stride = cache.batch_stride;
                 obj.fs = (cache.Fs) / 1000;
-                obj.type = cache.time_transform.type;
                 obj.f1 = cache.time_transform.f1;
                 obj.f2 = cache.time_transform.f2;
-                obj.minPCA = cache.time_transform.min_PCA;
-                obj.maxPCA = cache.time_transform.max_PCA;
                 disp('done.')
 
             elseif isfile(fullfile(path, [obj.main_foldername, '.mat']))
@@ -121,11 +103,8 @@ classdef ToolBoxClass < handle
                 load(fullfile(path, [obj.main_foldername, '.mat']), 'cache');
                 obj.stride = cache.batch_stride;
                 obj.fs = (cache.Fs) / 1000; %conversion in kHz
-                obj.type = cache.time_transform.type;
                 obj.f1 = cache.time_transform.f1;
                 obj.f2 = cache.time_transform.f2;
-                obj.minPCA = cache.time_transform.min_PCA;
-                obj.maxPCA = cache.time_transform.max_PCA;
                 disp('done.')
             elseif isfile(fullfile(path, 'Holovibes_rendering_parameters.json'))
                 disp('reading cache parameters from holovibes');
@@ -133,21 +112,15 @@ classdef ToolBoxClass < handle
                 footer_parsed = jsondecode(json_txt);
                 obj.stride = footer_parsed.compute_settings.image_rendering.time_transformation_stride;
                 obj.fs = footer_parsed.info.input_fps/1000; %conversion in kHz
-                obj.type = 'FFT';
                 obj.f1 = nan;
                 obj.f2 = nan;
-                obj.minPCA = nan;
-                obj.maxPCA = nan;
                 disp('done.')
             else
                 disp('WARNING : no rendering parameters file found');
                 obj.stride = 0;
                 obj.fs = 0;
-                obj.type = 'None';
                 obj.f1 = 0;
                 obj.f2 = 0;
-                obj.minPCA = 0;
-                obj.maxPCA = 0;
             end
 
             if isfile(fullfile(path, 'log', 'RenderingParameters.json')) % copies a simple log version for readability
@@ -155,22 +128,12 @@ classdef ToolBoxClass < handle
             end
             %% Calculation of the Sacling Factors
 
-            %           obj.ScalingFactorVelocityInPlane = 1000 * 1000 * PW_params.lambda / PW_params.opticalIndex * (3/PW_params.theta)^(1/2); % 1000 for kHz -> Hz and 1000 for m -> mm
-            %           obj.ScalingFactorVelocityInPlane = 30;
+            % obj.ScalingFactorVelocityInPlane = 1000 * 1000 * PW_params.lambda / PW_params.opticalIndex * (3/PW_params.theta)^(1/2); % 1000 for kHz -> Hz and 1000 for m -> mm
+            % obj.ScalingFactorVelocityInPlane = 30;
             obj.ScalingFactorVelocityInPlane = 1000 * 1000 * 2 * PW_params.lambda / sin(PW_params.phi);
             % obj.ScalingFactorVelocityInPlane = 1000 * 1000 * 2 * 2.4 * PW_params.lambda / 1.33;
-            obj.ScalingFactorVelocityCRA_AVG = 1000 * 1000 * PW_params.lambda / PW_params.opticalIndex * (PW_params.theta / 2); % 1000 for kHz -> Hz and 1000 for m -> mm
-            obj.ScalingFactorVelocityCRA_RMS = 1000 * 1000 * PW_params.lambda / PW_params.opticalIndex * (1 / (2 + 2 * (PW_params.theta ^ 3) / 3)) ^ (1/2); % 1000 for kHz -> Hz and 1000 for m -> mm
-            %% Parameters the color maps
-
-            obj.ARI_hue_max = 0;
-            obj.ARI_hue_min = 0.15;
-            obj.ARI_inflexion_point_hue = 0.7;
-            obj.ARI_slope_hue = 10;
-            obj.ARI_val_max = 0.4;
-            obj.ARI_val_min = 1;
-            obj.ARI_inflexion_point_val = 1;
-            obj.ARI_slope_val = 10;
+            % obj.ScalingFactorVelocityCRA_AVG = 1000 * 1000 * PW_params.lambda / PW_params.opticalIndex * (PW_params.theta / 2); % 1000 for kHz -> Hz and 1000 for m -> mm
+            % obj.ScalingFactorVelocityCRA_RMS = 1000 * 1000 * PW_params.lambda / PW_params.opticalIndex * (1 / (2 + 2 * (PW_params.theta ^ 3) / 3)) ^ (1/2); % 1000 for kHz -> Hz and 1000 for m -> mm
 
             % Turn On Diary Logging
             diary off
