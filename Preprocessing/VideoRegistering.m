d1c56d904db5891766500b5cfbd527661e68101b
Yann Fischer <yann.fischer.pro@gmail.com>
2025-01-20 15:04:06 +0100

    Video Registering Fix

--- Preprocessing/VideoRegistering.m
+++ Preprocessing/VideoRegistering.m
@@ -10,11 +10,11 @@ function obj = VideoRegistering(obj)
     video = obj.M0_ff_video;
     numX = size(video, 1);
     numY = size(video, 2);
-    x = linspace(-numX / 2, numX / 2, numX);
-    y = linspace(-numY / 2, numY / 2, numY);
+    x = linspace(-numX / 2, numX / 2, numY);
+    y = linspace(-numY / 2, numY / 2, numX);
     [X, Y] = meshgrid(x, y);
 
-    disc_ratio = 0.7; % parametrize this coef if needed
+    disc_ratio = 0.9; % parametrize this coef if needed
     disc = X .^ 2 + Y .^ 2 < (disc_ratio * min(numX, numY) / 2) ^ 2;
     video_reg = video .* disc - disc .* sum(video .* disc, [1, 2]) / nnz(disc); % minus the mean in the disc of each frame
     video_reg = reshape(video_reg, size(video, 1), size(video, 2), 1, size(video, 3)); % insert a dimension to match reegistration functions
