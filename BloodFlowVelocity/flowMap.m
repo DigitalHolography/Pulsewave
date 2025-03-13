40fa58d25f5db3067d266c25093917d7d6db3f39
Yann Fischer <yannfisch3r@gmail.com>
2025-03-13 10:29:54 +0100

    subfunction for flowMap

--- BloodFlowVelocity/flowMap.m
+++ BloodFlowVelocity/flowMap.m
@@ -9,8 +9,12 @@ function [v_video_RGB, v_mean_RGB] = flowMap(v_video, maskSection, maskArtery, m
     M0_ff_image = rescale(mean(M0_ff_video, 3));
     [numX, numY, numFrames] = size(v_video);
 
-    % Precompute masks
-    maskAV = maskArtery & maskVein;
+    if veinsAnalysis
+        % Precompute masks
+        maskAV = maskArtery & maskVein;
+        maskArtery = maskArtery & ~maskAV;
+        maskVein = maskVein & ~maskAV;
+    end
 
     % Precompute constants
     x_c = xy_barycenter(1) / numX;
@@ -26,8 +30,10 @@ function [v_video_RGB, v_mean_RGB] = flowMap(v_video, maskSection, maskArtery, m
 
     % Precompute colormaps
     cmapArtery = cmapLAB(256, [0 0 0], 0, [1 0 0], 1/3, [1 1 0], 2/3, [1 1 1], 1);
-    cmapVein = cmapLAB(256, [0 0 0], 0, [0 0 1], 1/3, [0 1 1], 2/3, [1 1 1], 1);
-    cmapAV = cmapLAB(256, [0 0 0], 0, [1 0 1], 1/3, [1 1 1], 1);
+    if veinsAnalysis
+        cmapVein = cmapLAB(256, [0 0 0], 0, [0 0 1], 1/3, [0 1 1], 2/3, [1 1 1], 1);
+        cmapAV = cmapLAB(256, [0 0 0], 0, [1 0 1], 1/3, [0 1 0], 2/3, [1 1 1], 1);
+    end
 
     % Precompute v_rescaled
     v_max = max(v_video(maskSection));
@@ -38,44 +44,42 @@ function [v_video_RGB, v_mean_RGB] = flowMap(v_video, maskSection, maskArtery, m
     % Precompute v_mean and v_mean_rescaled
     v_mean = squeeze(mean(v_video, 3));
     v_mean_rescaled = squeeze(mean(v_rescaled, 3));
-
-    velocityIm(v_mean, maskArtery, cmapArtery, 'arteries', colorbarOn = true);
-    velocityColorbar(cmapArtery, v_min, v_max, 'Arteries');
-
-    v_mean_Artery = setcmap(v_mean_rescaled, maskArtery, cmapArtery);
-
     v_video_RGB = zeros(numX, numY, 3, numFrames);
 
     if veinsAnalysis
-        velocityIm(v_mean, (maskArtery | maskVein), turbo, 'vessels', colorbarOn = true);
-
+        % Velocity Images
+        velocityIm(v_mean, maskArtery, cmapArtery, 'arteries', colorbarOn = true);
         velocityIm(v_mean, maskVein, cmapVein, 'veins', colorbarOn = true);
-        velocityColorbar(cmapVein, v_min, v_max, 'Veins');
+        velocityIm(v_mean, maskArtery | maskVein, turbo, 'vessels', colorbarOn = true);
 
-        v_mean_Vein = setcmap(v_mean_rescaled, maskVein, cmapVein);
-        v_mean_AV = setcmap(v_mean_rescaled, (maskArtery & maskVein), cmapAV);
-        v_mean_RGB = (v_mean_Artery + v_mean_Vein) .* ~maskAV + v_mean_AV + M0_ff_image .* ~(maskArtery | maskVein);
-        v_mean_RGB = v_mean_RGB .* ~circles + circles .* 0.7;
+        % velocity Colorbars of the gif
+        velocityColorbar(cmapArtery, v_min, v_max, 'Arteries');
+        velocityColorbar(cmapVein, v_min, v_max, 'Veins');
+        
+        % Average Image
+        v_mean_RGB = flowMapImg(v_mean_rescaled, {maskArtery, maskVein, maskAV}, {cmapArtery, cmapVein, cmapAV} ,background = M0_ff_image, circles = circles);
         imwrite(v_mean_RGB, fullfile(TB.path_png, 'bloodFlowVelocity', sprintf("%s_%s", TB.main_foldername, 'v_mean.png')))
 
+        % Gif frame generation
         parfor frameIdx = 1:numFrames
-            v_frame_Artery = setcmap(v_rescaled(:, :, frameIdx), maskArtery, cmapArtery);
-            v_frame_Vein = setcmap(v_rescaled(:, :, frameIdx), maskVein, cmapVein);
-            v_mean_AV = setcmap(v_rescaled(:, :, frameIdx), (maskArtery & maskVein), cmapAV);
-            v_video_RGB(:, :, :, frameIdx) = (v_frame_Artery + v_frame_Vein) .* ~maskAV + v_mean_AV + M0_ff_video(:, :, frameIdx) .* ~(maskArtery | maskVein);
-            v_video_RGB(:, :, :, frameIdx) = v_video_RGB(:, :, :, frameIdx) .* ~circles + circles .* 0.7;
+            v_video_RGB(:, :, :, frameIdx) = flowMapImg(v_rescaled(:, :, frameIdx), {maskArtery, maskVein, maskAV}, {cmapArtery, cmapVein, cmapAV} ,background = M0_ff_video(:, :, frameIdx), circles = circles);
         end
 
     else
 
-        v_mean_RGB = v_mean_Artery .* maskArtery + M0_ff_image .* ~maskArtery;
-        v_mean_RGB = v_mean_RGB .* ~circles + circles .* 0.7;
+        % Velocity Images
+        velocityIm(v_mean, maskArtery, cmapArtery, 'arteries', colorbarOn = true);
+
+        % velocity Colorbars of the gif
+        velocityColorbar(cmapArtery, v_min, v_max, 'Arteries');
+
+        % Average Image
+        v_mean_RGB = flowMapImg(v_mean_rescaled, {maskArtery}, {cmapArtery} ,background = M0_ff_image, circles = circles);
         imwrite(v_mean_RGB, fullfile(TB.path_png, 'bloodFlowVelocity', sprintf("%s_%s", TB.main_foldername, 'v_mean.png')))
 
+        % Gif frame generation
         parfor frameIdx = 1:numFrames
-            v_frame_Artery = setcmap(v_rescaled(:, :, frameIdx), maskArtery, cmapArtery);
-            v_video_RGB(:, :, :, frameIdx) = v_frame_Artery + M0_ff_video(:, :, frameIdx) .* ~maskArtery;
-            v_video_RGB(:, :, :, frameIdx) = v_video_RGB(:, :, :, frameIdx) .* ~circles + circles .* 0.7;
+            v_video_RGB(:, :, :, frameIdx) = flowMapImg(v_rescaled(:, :, frameIdx), {maskArtery}, {cmapArtery} ,background = M0_ff_video(:, :, frameIdx), circles = circles);
         end
 
     end
